0f9ee00b19a8f44fe3d09ec326c2ea1cdb80ccde
Martyn <Martyn.bell@gmail.com>
2020-07-26 19:56:57 +0100

    Seconf one

--- quickstart.py
+++ quickstart.py
@@ -5,6 +5,8 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+# MADE THIS CHANGE
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
